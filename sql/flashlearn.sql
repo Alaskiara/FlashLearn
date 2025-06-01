996d3a7a1f3ba24866093bf353b7ff455db03321
Luca Brezina <if24b008@technikum-wien.at>
2025-06-01 15:59:42 +0200

    DB überarbeitet

--- sql/flashlearn.sql
+++ sql/flashlearn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 07. Apr 2025 um 21:23
+-- Erstellungszeit: 01. Jun 2025 um 15:57
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -29,11 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `card` (
   `Card_ID` int(11) NOT NULL,
+  `User_ID` int(5) NOT NULL,
   `Frage` text NOT NULL,
   `Antwort` text NOT NULL,
   `points` int(11) NOT NULL,
   `Kategorie_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Daten für Tabelle `card`
+--
+
+INSERT INTO `card` (`Card_ID`, `User_ID`, `Frage`, `Antwort`, `points`, `Kategorie_ID`) VALUES
+(1, 1, 'test', 'test', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -45,6 +53,14 @@ CREATE TABLE `kategorie` (
   `Kategorie_ID` int(11) NOT NULL,
   `Bezeichnung` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Daten für Tabelle `kategorie`
+--
+
+INSERT INTO `kategorie` (`Kategorie_ID`, `Bezeichnung`) VALUES
+(1, 'Mathe'),
+(2, 'OOP');
 
 -- --------------------------------------------------------
 
@@ -60,6 +76,16 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`User_ID`, `username`, `passwort`, `punkte`) VALUES
+(1, 'test', '$2y$10$R3LuyU8S8/jSOxi4ZHLfoetKOcVDpJf9xvCbTvqzZg9GWyc9yv.YO', 0),
+(2, 'abc', '$2y$10$Aj1i6Vsk07UoL4CYdZmj9e52qAyCktF/BhQb7kZUdNDG73jigmozy', 0),
+(3, 'test2', '$2y$10$KNIW7icTJfJ354mhSNnnZOEqz.1LnFTgtH3QvIHCPvzvS1H1ntg2W', 0),
+(4, 'asdf', '$2y$10$au6N2n68PshyGIHGm3qQGOFII2mi6fFqT/F856vc0RJoOXcdYc1re', 0);
+
+--
 -- Indizes der exportierten Tabellen
 --
 
@@ -67,7 +93,9 @@ CREATE TABLE `user` (
 -- Indizes für die Tabelle `card`
 --
 ALTER TABLE `card`
-  ADD PRIMARY KEY (`Card_ID`);
+  ADD PRIMARY KEY (`Card_ID`),
+  ADD KEY `User_ID` (`User_ID`),
+  ADD KEY `Kategorie_ID` (`Kategorie_ID`);
 
 --
 -- Indizes für die Tabelle `kategorie`
@@ -89,19 +117,30 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `card`
 --
 ALTER TABLE `card`
-  MODIFY `Card_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Card_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT für Tabelle `kategorie`
 --
 ALTER TABLE `kategorie`
-  MODIFY `Kategorie_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Kategorie_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `card`
+--
+ALTER TABLE `card`
+  ADD CONSTRAINT `Kategorie_ID` FOREIGN KEY (`Kategorie_ID`) REFERENCES `kategorie` (`Kategorie_ID`),
+  ADD CONSTRAINT `User_ID` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
